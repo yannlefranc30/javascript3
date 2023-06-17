5324f1ad1ff9d8db1f4698b5b135065810c368c6
yann <yannlefranc330@gmail.com>
2023-06-17 10:15:47 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">D'accord</button>
+
+<p id="demo"></p>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
